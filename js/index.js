67e64fbd9705dd3b09e7dfb19fb667105df4430f
AndrianKrilyshyn <148096063+AndrianKrilyshyn@users.noreply.github.com>
2024-02-05 14:35:51 +0000

    Add js for index-form-partial

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ document.addEventListener("partialsLoaded", () => {
   import("./header.js");
   import("./index-hero-block.js");
   import("./weekly-carousel.js");
+  import("./hire-us-form.js");
 });
